1cacdcc68a83e53856aeee02c441a4c877aacdb2
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2023-12-13 12:16:28 +0500

    JS first class

---
+++ app.js
@@ -0,0 +1,49 @@
+alert("hello javasriprt, it is for user only")
+
+//console.log("it is for programmer;s comment")
+
+var person= "zaid";
+
+//var 10person (illegal)
+// var person10 (legal)
+// var person-1 (illegal)
+// var person_1 (legal)
+// var $person(illegal)
+// var per%son (illegal)
+// var alert (illegal)
+// var person name (illegal)
+// var per!son (illegal)
+// var pers*son (illegal)
+
+
+// alert(person)
+
+// var marks=75
+// console.log (marks)
+
+// var firstName = "sharjeel"
+
+// console.log (firstName)
+
+
+var a=10;
+// var b=a+1;
+// var b=++a;
+ var b=a++
+// var b=--a
+
+// console.log(a, "A value")
+// var b=a--
+// var b=--a
+
+// var b=--a*(a*2)
+console.log(b,"B value")
+console.log(a, "A value")
+
+
+// Prompt practise
+
+
+// var userName=prompt("Enter your name");
+// var age=prompt("enter your age");
+// console.log(userName,age);
